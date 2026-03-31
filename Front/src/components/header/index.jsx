3a1e5ddf3195eb0850fd694f2287f2617ead1639
João Victor Carvalho <joaovictor.crvl@gmail.com>
2026-03-31 19:51:35 -0300

    cabecalho

--- Front/src/components/header/index.jsx
+++ Front/src/components/header/index.jsx
@@ -5,28 +5,63 @@ function Header() {
         <header>
             <section className="cabecalho">
                 <div className="conteudo-90">
-                    <div className="perfil">
-                        <div className="imagem-container">
-                            <img src="src/assets/logo.png" alt="" className="foto_perfil" />
-                        </div>
-                        <div className="textos">
-                            <div className="nome">
-                                <p>UserName</p>
+                    <div className="conteudo">
+                        <div className="perfil">
+                            <div className="imagem-container">
+                                <img src="" alt="" className="foto_perfil" />
                             </div>
-                            <div className="cargo">
-                                <p>Administrador</p>
+                            <div className="textos">
+                                <div className="nome">
+                                    <p>UserName</p>
+                                </div>
+                                <div className="cargo">
+                                    <p>Administrador</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="logo">
+                            <div className="imagem-container">
+                                <img src="src/assets/logo.png" alt="" />
+                            </div>
+                        </div>
+                        <div className="funcionalidades">
+                            <div className="imagem-container">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="imagem-container">
+                                <img src="" alt="" />
                             </div>
                         </div>
                     </div>
-                    <div className="logo"></div>
-                    <div className="funcionalidades">
-                        <div className="imagem-container">
-                            <img src="" alt="" />
+                </div>
+            </section>
+            <section className='navegacao'>
+                <div className='lista-link'>
+                    <a href="" className='link'>
+                        <div className="texto"><p>Eventos</p></div>
+                        <div className="icone">
                         </div>
-                        <div className="imagem-container">
-                            <img src="" alt="" />
+                    </a>
+                    <a href="" className='link'>
+                        <div className="texto"><p>Eventos</p></div>
+                        <div className="icone">
                         </div>
-                    </div>
+                    </a>
+                    <a href="" className='link'>
+                        <div className="texto"><p>Eventos</p></div>
+                        <div className="icone">
+                        </div>
+                    </a>
+                    <a href="" className='link'>
+                        <div className="texto"><p>Eventos</p></div>
+                        <div className="icone">
+                        </div>
+                    </a>
+                    <a href="" className='link'>
+                        <div className="texto"><p>Eventos</p></div>
+                        <div className="icone">
+                        </div>
+                    </a>
                 </div>
             </section>
         </header>
